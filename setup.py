cadfabf5730e87c13d1bd73e01b8e9d9372ae3da
archanjash07 <archanjash@gmail.com>
2021-01-16 22:18:28 +0530

    added dist packages for usage

--- setup.py
+++ setup.py
@@ -4,13 +4,12 @@ setuptools.setup(
     name="shiftencode", # Replace with your own username
     version="0.0.1",
     author="Dip Ghosh",
-    author_email="author@example.com",
-    description="A small example package",
-    long_description="long_description",
+    author_email="dipghoshraj@gmail.com",
+    description="shift-encode is a encoding and decoding libery for text and messages",
+    long_description= open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/dipghoshraj/shiftencode",
     packages=setuptools.find_packages(),
-    package_dir={'':'shiftencode'},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
